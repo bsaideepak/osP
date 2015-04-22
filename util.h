0752cabc339315cd99db0ed49d8657e08f16fdaf
Saideepak Budaraju <saideepak09@gmail.com>
2015-04-22 12:48:42 -0700

    ...

---
+++ util.h
@@ -0,0 +1,24 @@
+/************************************************
+ * Operating Systems CSE 430
+ *
+ * Copyright (c) 2015
+ *      Sydney Vanda
+ *      Brittney RUssell
+ *
+ *  Module Name:
+ *      \file util.h
+ *
+ *  Description:
+ *      \brief Include all of the necessary header files to avoid redeclaration.
+ *
+ ***********************************************/
+#ifndef UTIL_H_
+#define UTIL_H_
+
+#include "threads.h"
+#include "sem.h"
+#include "q.h"
+#include "TCB.h"
+#include "ucontext.h"
+
+#endif
